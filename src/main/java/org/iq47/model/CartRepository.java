1cd7ccce22fe41021a6ee5eb7ae2d59d8722fc3e
spynad <DanyPs1@yandex.ru>
2022-12-12 23:42:23 +0300

    Add some more cart endpoints & order/orderaddress stubs

--- src/main/java/org/iq47/model/CartRepository.java
+++ src/main/java/org/iq47/model/CartRepository.java
@@ -1,9 +1,15 @@
 package org.iq47.model;
 
 import org.iq47.model.entity.Cart;
+<<<<<<< HEAD
+=======
+import org.iq47.model.entity.ShopItem;
+>>>>>>> caeb03c (Add some more cart endpoints & order/orderaddress stubs)
 import org.iq47.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
